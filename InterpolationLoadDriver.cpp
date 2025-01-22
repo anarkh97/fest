92cc0e568f40a5b054fa775cdeb511fe415f255c
Aditya Narkhede <anarkhede@vt.edu>
2025-01-22 17:44:20 -0500

    The product of constant pressure and area is computed by the InterpolationLoadDriver for testing purposes before implementing the interpolation features.

--- InterpolationLoadDriver.cpp
+++ InterpolationLoadDriver.cpp
@@ -114,7 +114,85 @@ void
 InterpolationLoadDriver::ComputeForces(TriangulatedSurface &surface, vector<Vec3D> &force, 
                                        vector<Vec3D> *force_over_area, double t)
 {
-  //
+  // pass a constant pressure * area to aero-s for testing.
+
+  // split the job among the processors (for parallel interpolation)
+  // AN: reused from M2C DynamicLoadCalculator::InterpolateInSpace
+  int active_nodes = surface.active_nodes;
+  int active_elems = surface.active_elems;
+
+  assert(active_nodes == (int)force.size());
+
+  int mpi_rank, mpi_size;
+  MPI_Comm_rank(comm, &mpi_rank);
+  MPI_Comm_size(comm, &mpi_size);
+
+  int elems_per_rank = active_elems / mpi_size;
+  int remainder = active_nodes - elems_per_rank; // left-over nodes.
+
+  assert(remainder >= 0 and remainder < mpi_size); 
+
+  vector<int> counts(mpi_size, -1);      // stores the number of elements each rank is responsible for.
+  vector<int> start_index(mpi_size, -1); // stores the starting index for each rank in the surface/force vectors.
+
+  for(int i=0; i<mpi_size; ++i) {
+
+    // ranks 0 -- remainder - 1 handle an extra node.
+    counts[i] = (i < remainder) ? elems_per_rank + 1 : elems_per_rank;
+    start_index[i] = (i < remainder) ? (elems_per_rank + 1)*i : elems_per_rank*i + remainder;
+
+  }
+
+  assert(start_index.back() + counts.back() == active_elems);
+
+  int my_elem_size = counts[mpi_rank];
+  int my_start_index = start_index[mpi_rank];
+
+  // clear old force values
+  for(int index=my_start_index; index<my_elem_size; ++index) {
+
+    // this element's nodes
+    int node1 = surface.elems[index][0];
+    int node2 = surface.elems[index][1];
+    int node3 = surface.elems[index][2];
+
+    // clear old values in the force vector
+    force[node1] = Vec3D(0.);
+    force[node2] = Vec3D(0.);
+    force[node3] = Vec3D(0.);
+
+  }
+
+  // compute forces
+  for(int index=my_start_index; index<my_elem_size; ++index) {
+
+    // this element's nodes
+    int node1 = surface.elems[index][0];
+    int node2 = surface.elems[index][1];
+    int node3 = surface.elems[index][2];
+
+    // calculate force
+    double area  = surface.elemArea[index]/3;
+    Vec3D normal = surface.elemNorm[index];
+
+    force[node1] += (1e6)*area*normal;
+    force[node2] += (1e6)*area*normal;
+    force[node3] += (1e6)*area*normal;
+
+    if(force_over_area) {
+      // Reduce operation adds up force over area from all processors.
+      (*force_over_area)[node1] = 1e6 / mpi_size;
+      (*force_over_area)[node2] = 1e6 / mpi_size;
+      (*force_over_area)[node3] = 1e6 / mpi_size;
+    }
+
+  }
+
+  // communication
+  MPI_Allreduce(MPI_IN_PLACE, (double*)force.data(), active_nodes*3, MPI_DOUBLE, MPI_SUM, comm);
+  if(force_over_area)
+    MPI_Allreduce(MPI_IN_PLACE, (double*)force_over_area->data(), active_nodes*3, MPI_DOUBLE, MPI_SUM, comm);
+
 }
 
 //------------------------------------------------------------
